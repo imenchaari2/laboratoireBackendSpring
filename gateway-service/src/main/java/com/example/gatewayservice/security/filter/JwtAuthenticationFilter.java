3e4c85d7ea5cddc65f44e50d70558307e1380866
imen chaari <imen.chaari@enis.tn>
2022-12-05 23:12:11 +0100

    configure cors

--- gateway-service/src/main/java/com/example/gatewayservice/security/filter/JwtAuthenticationFilter.java
+++ gateway-service/src/main/java/com/example/gatewayservice/security/filter/JwtAuthenticationFilter.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
